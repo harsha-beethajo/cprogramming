f180548e91187c58402d63751396fb763b074e07
harsha-beethajo <harshabeethajo@gmail.com>
2026-07-21 14:37:20 +0530

    adding files

--- arrays/4x4_arrays.c
+++ arrays/4x4_arrays.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 // Function -1
-int numbers[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+int numbers[4][4] = {{1, 3, 2, 4}, {5, 7, 6, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+int final_box[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+
 int j = 0;
+int i = 0;
+int k = 0;
+
+void gotoxy(int x, int y)
+{
+    COORD pos = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void hideCursor(void)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(h, &info);
+}
+
 void F1()
 {
     printf("%c", 201);
-    for (int i = 1; i <= 19; i++)
+    for (i = 1; i <= 19; i++)
         i % 5 != 0 ? printf("%c", 205) : printf("%c", 203);
     printf("%c\n", 187);
 }
@@ -13,8 +35,8 @@ void F1()
 
 void F2()
 {
-    int k = 0;
-    for (int i = 0; i <= 20; i++)
+    k = 0;
+    for (i = 0; i <= 20; i++)
     {
         if (i % 5 == 0)
         {
@@ -22,7 +44,8 @@ void F2()
         }
         else if (i % 5 == 1)
         {
-            printf("%3d", numbers[j][k]);
+            (numbers[j][k] == 0) ? printf("%3c", 32)
+                                 : printf("%3d", numbers[j][k]);
             i += 2;
             k++;
         }
@@ -30,14 +53,14 @@ void F2()
             printf("%c", 32);
     }
     printf("\n");
-    j++;  
+    j++;
 }
 
 // Function -3
 void F3()
 {
     printf("%c", 204);
-    for (int i = 1; i <= 19; i++)
+    for (i = 1; i <= 19; i++)
         i % 5 != 0 ? printf("%c", 205) : printf("%c", 206);
     printf("%c\n", 185);
 }
@@ -45,12 +68,82 @@ void F3()
 void F4()
 {
     printf("%c", 200);
-    for (int i = 1; i <= 19; i++)
+    for (i = 1; i <= 19; i++)
         i % 5 != 0 ? printf("%c", 205) : printf("%c", 202);
     printf("%c\n", 188);
 }
-int main()
+
+int getkey()
+{
+    int ch = getch();
+    // if (ch == 0 || ch == 224)
+    //     ch = getch();
+    //    return ch;
+    return (ch == 0 || ch == 224) ? getch() : ch;
+}
+
+void swapping(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void box()
 {
     F1(), F2(), F3(), F2(), F3(), F2(), F3(), F2(), F4();
+}
+
+
+
+int main()
+{
+    int row0 = 3;
+    int column0 = 3;
+    hideCursor();
+    system("cls");
+    box();
+    int key = getkey();
+
+    while (key == 72 || key == 75 || key == 77 || key == 80)
+    {
+
+        i = 0, j = 0, k = 0;
+        key = getkey();
+        if (key == 80 && row0 > 0 /*Move down*/)
+        {
+            swapping(&numbers[row0][column0], &numbers[row0 - 1][column0]);
+            row0--;
+        }
+
+        if (key == 72 && row0 < 3 /*Move up*/)
+        {
+            swapping(&numbers[row0][column0], &numbers[row0 + 1][column0]);
+            row0++;
+        }
+        if (key == 77 && column0 > 0 /*move right*/)
+        {
+            swapping(&numbers[row0][column0], &numbers[row0][column0 - 1]);
+            column0--;
+        }
+        if (key == 75 && column0 < 3)
+        {
+            swapping(&numbers[row0][column0], &numbers[row0][column0 + 1]);
+            column0++;
+        }
+
+        if (final_box == numbers)
+        {
+            // printf ("Success ");
+            // exit
+        }
+        // system("cls");
+        // printf("\033[2J\033[H");
+        gotoxy(0, 0);
+        box();
+        fflush(stdout);
+    }
+
     return 0;
 }
