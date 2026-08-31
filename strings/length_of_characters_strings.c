223754f0333b7a2b8c77b4ef34442a2bf9c1696b
harsha-beethajo <harshabeethajo@gmail.com>
2026-08-31 19:14:38 +0530

    adding new & modifying old programs

--- strings/length_of_characters_strings.c
+++ strings/length_of_characters_strings.c
@@ -4,21 +4,32 @@ int LENGTH(char *str)
 {
     int i = 0;
     while (str[i] != '\0')
-    i++;
+        i++;
     return i;
 }
 int main()
 {
     char name[size];
     gets(name);
-    /* 
-   THIS is without the use of function... 
-    
+    /*
+   THIS is without the use of function...
+
     int i=0;
     while (name[i]!='\0')
     i++;
     printf("%d",i); */
-    
-    printf("%d", LENGTH (name));
+    int i = 0;
+    int space_count = 0;
+    for (i = 0; i < size; i++)
+    {
+
+        if (name[i] == ' ')
+        {
+            space_count++;
+        }
+    }
+
+    printf("%d\n", space_count);
+    printf("Length of characters: %d", LENGTH(name) - space_count);
     return 0;
 }
